2c9a182cd2221097b21b7232b04f23fa0e6231cb
OlivaEduardo <lokasocba@gmail.com>
2018-03-07 21:42:19 -0300

    ...

--- Practico2/Main/Operaciones_logicas.h
+++ Practico2/Main/Operaciones_logicas.h
@@ -3,4 +3,18 @@
 #ifndef OP_LOGICAS_H
 #define OP_LOGICAS_H
 
+void swap(int* px, int* py ) 
+{
+	int aux = *px;
+	*px = *py;
+	*py = aux;
+}
+
+void swap(double* px, double* py)
+{
+	double aux = *px;
+	*px = *py;
+	*py = aux;
+}
+
 #endif // !OP_LOGICAS_H
