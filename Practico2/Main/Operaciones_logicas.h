f854255e3e3a7ebcd616204f58628920ff930ab8
OlivaEduardo <lokasocba@gmail.com>
2018-03-09 01:59:04 -0300

    Ver despues

--- Practico2/Main/Operaciones_logicas.h
+++ Practico2/Main/Operaciones_logicas.h
@@ -3,18 +3,23 @@
 #ifndef OP_LOGICAS_H
 #define OP_LOGICAS_H
 
-void swap(int* px, int* py ) 
+void swap(int* ptrx, int* ptry ) 
 {
-	int aux = *px;
-	*px = *py;
-	*py = aux;
+	int aux = *ptrx;
+	*ptrx = *ptry;
+	*ptry = aux;
 }
 
-void swap(double* px, double* py)
+void swap(double* ptrx, double* ptry)
 {
-	double aux = *px;
-	*px = *py;
-	*py = aux;
+	double aux = *ptrx;
+	*ptrx = *ptry;
+	*ptry = aux;
+}
+
+void swap(char ptrx[], char ptry[])
+{
+
 }
 
 #endif // !OP_LOGICAS_H
